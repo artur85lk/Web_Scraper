a1daf1ba446dc816d5dad4725cef5c6f16478893
artur85lk <artur85lk@o2.pl>
2022-09-16 02:34:37 -0700

    3/5

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,26 +1,16 @@
 import requests
-from bs4 import BeautifulSoup
 
 print("Input the URL:")
-# https://web.archive.org/web/20220310110507/https://www.imdb.com/title/tt0080684/
-
 url = input()
-movie = {}
-r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-soup = BeautifulSoup(r.content, 'html.parser')
+page_content = requests.get(url)
 
-try:
-    paragraphs = soup.find('h1')
-    movie["title"] = paragraphs.text
+if page_content.status_code == 200:
+    with open("source.html", "wb") as f:
+        f.write(page_content.content)
 
-    description1 = soup.find_all('span',{'data-testid': 'plot-l'})
+    print("\nContent saved.")
+else:
+    print(f"\nThe URL returned {page_content.status_code}")
 
-    for i in description1:
-        movie["description"] = i.text
-    if movie["title"] != "" and movie["description"] != "":
-        print(movie)
-    else:
-        print("Invalid quote resource!")
-except Exception:
-    print()
-    print("Invalid movie page!")
+
+
